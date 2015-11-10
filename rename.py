725ffd75e18d41de9ee3d93c15645716b2f207d3
Alice <alicegutteridge@gmail.com>
2015-11-10 15:57:00 +0000

    Made general-purpose, added documentation

--- rename.py
+++ rename.py
@@ -1,3 +1,15 @@
+###############################################################################
+# For when your barcodes correspond to the wrong samples!
+#
+# Make sure this is executed from the run directory.
+#
+# Ensure the input file is named "sample_data.txt" and is in the same
+# directory, with 4 tab-separated columns corresponding to:
+# 0: old sample number
+# 1: new sample number
+# 2: old sample name
+# 3: new sample name
+
 import os
 import subprocess
 import re
@@ -21,11 +33,8 @@ def log(message):
         datafile.close()
 
 
-# Takes in tab-separated list of:
-# 0: old sample number
-# 1: new sample number
-# 2: old sample name
-# 3: new sample name
+# Reads the input sample information and creates a dictionary for both sample
+# names and sample numbers separately.
 def read_dicts():
     sample_names = dict()
     sample_nums = dict()
@@ -47,13 +56,14 @@ def main():
     if sample_names and sample_nums:
         print("dictionary populated.")
 
-    root = os.getcwd() + "/BaseSpace/cranio1-26049053/"
+    root = os.getcwd()
 
     for dirname in os.listdir(root):
         # To check it is dir, not file
         if os.path.isdir(root + dirname):
             bcpath = root + dirname + "/Data/Intensities/BaseCalls/"
             for filename in os.listdir(bcpath):
+                # Renaming files in directory
                 # get sample number between S1-96
                 num_re = re.match(r"\S*_S(\d+)\S*",
                                   filename)
@@ -69,24 +79,22 @@ def main():
                                   bcpath + new_filename)
                     except KeyError:
                         log(old_num + " not found in dict!")
-                else:
-                    if (".sam" in filename or
-                            ".bam" in filename or
-                            ".fastq" in filename):
+                elif (".sam" in filename or
+                      ".bam" in filename or
+                      ".fastq" in filename):
                         mv_rename(bcpath + filename,
                                   bcpath + filename + "_old_kept")
 
-                if filename is ".DS_Store":
-                    subprocess.check_call(["rm", filename])
-
-            name_re = re.match(r"([\S*\s*]*)-300\S*",
+            # Renaming directory
+            # Assumes Illumina numeric ID is at least 8 digits long
+            name_re = re.match(r"([\S*\s*]*)-\d\d\d\d\d\d\d\d+\Z",
                                dirname)
 
             if name_re:
                 old_name = name_re.group(1)
                 try:
                     new_name = sample_names.get(old_name)
-                    new_dirname = re.sub(r"([\S*\s*]*)(-300\S*)",
+                    new_dirname = re.sub(r"([\S*\s*]*)(-\d\d\d\d\d\d\d\d+)\Z",
                                          new_name + r"\g<2>",
                                          dirname)
                     mv_rename(root + dirname,
@@ -96,9 +104,6 @@ def main():
             else:
                 mv_rename(root + dirname,
                           root + dirname + "_old_kept")
-
-        if dirname is ".DS_Store":
-            subprocess.check_call(["rm", dirname])
 
 if __name__ == "__main__":
     main()
