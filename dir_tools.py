47e5adb287535419fe57cb7e6962f2cd7262cef7
Alice <alicegutteridge@gmail.com>
2015-11-10 17:04:32 +0000

    Made regex for run dir more specific.

--- dir_tools.py
+++ dir_tools.py
@@ -3,30 +3,42 @@ import sys
 import re
 
 
-def get_all_paths():
+def get_all_paths(args_list):
     run_dir = ""
+    run_name = ""
 
+    # Requires path to run directory to be supplied as a command line argument.
     try:
-        run_dir = sys.argv[1]
+        run_dir = args_list[1]
+
         if not os.path.isdir(run_dir):
             raise OSError("Not a valid path.")
+
+        # adding forward slash at end
+        if run_dir[-1] is not "/":
+            run_dir = run_dir + "/"
+
+        # get run name i.e. last dir
+        # Assumes Illumina numeric ID is at least 8 digits long
+        run_re = re.match(r"([\S*\s*]*)-\d\d\d\d\d\d\d\d/+\Z", run_dir)
+
+        if run_re:
+            run_name = run_re.group(1)
+        else:
+            raise AttributeError("Not a run directory.")
     except IndexError:
-        print("""Please give the path to the run directory as a command line
-               argument.""")
+        print("Please give the path to the run directory as a command line" +
+              " argument.")
         sys.exit(0)
-    except OSError as err:
-        print(err.args)
+    except OSError as oe:
+        for e in oe.args:
+            print(e)
+        sys.exit(0)
+    except AttributeError as ae:
+        for e in ae.args:
+            print(e)
         sys.exit(0)
 
-    # adding forward slash at end
-    if run_dir[-1] is not "/":
-        run_dir = run_dir + "/"
-
-    # get run name i.e. last dir
-    run_name = ""
-    run_re = re.match(r"[\S*\s*]*/([\S*\s*]*)/\Z", run_dir)
-
-    run_name = run_re.group(1)
     root = os.getcwd() + "/"
     return ({"root": root,
              "run_name": run_name,
