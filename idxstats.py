7f3f345d1e757b6d75e7ef67fa42e28664ab39ef
Alice <alicegutteridge@gmail.com>
2015-11-11 00:03:31 +0000

    creating subset for chrs of interest

--- idxstats.py
+++ idxstats.py
@@ -21,31 +21,42 @@ def main():
                         to_write.append(info["sample_num"])
                         to_write.append(info["sample_name"])
 
+                        # subset!!!
+                        subprocess_args = [samtools view -Shu -L cranio1.bed S34_sorted.bam > S34_sorted_subset.bam]
+                        index_args = [samtools index S34_sorted_subset.bam]
+
+                        subprocess.check_call()
+
                         popen_args = ["samtools",
                                       "idxstats",
                                       data_path + filename]
 
-                        # different to CollectAlignmentSummaryMetrics per chr ?
                         p = subprocess.Popen(popen_args, stdout=subprocess.PIPE)
 
                         while 1:
                             row = p.stdout.readline()
                             row_list = row.decode('UTF-8').split("\t")
-                            if (row_list[1] is not "0"
-                                    or row_list[2] is not "0"):
-                                mapped = row_list[1]
-                                unmapped = row_list[2]
-                                to_write.append(row_list[0])
-                                to_write.append(mapped)
-                                to_write.append(unmapped)
-                                percent = int(mapped) / (int(unmapped) + int(mapped))
-                                to_write.append("{0:.1f}".format(percent * 100))
-                                print("{0:.1f}".format(percent * 100))
-                                sys.exit(0)
+                            if len(row_list) > 1:
+                                if (row_list[1] is not "0"
+                                        or row_list[2] is not "0"):
+                                    mapped = row_list[1]
+                                    unmapped = row_list[2]
+                                    to_write.append(row_list[0])
+                                    to_write.append(mapped)
+                                    to_write.append(unmapped)
+                                    percent = int(mapped) / (int(unmapped) + int(mapped))
+                                    to_write.append("{0:.1f}".format(percent * 100))
                             if not row and p.returncode is not None:
                                 break
                             p.poll()
                         print("done %d" % p.returncode)
+
+                        with open(os.path.join(os.getcwd(), 'idxstats.csv'), 'a') as datafile:
+                            for w in to_write:
+                                datafile.write(w + "\t")
+
+                            datafile.write("\n")
+                            datafile.close()
 
             # Iterate through files, produce idxstats, append to file!
             # for filename in temppath:
