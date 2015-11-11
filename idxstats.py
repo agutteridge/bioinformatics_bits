cf7ea6ab276785cc23470915e01e72c4fdcec3db
Alice <alicegutteridge@gmail.com>
2015-11-11 13:24:12 +0000

    Writes specific chromosomes to file

--- idxstats.py
+++ idxstats.py
@@ -7,6 +7,29 @@ import dir_tools
 
 def main():
     paths = dir_tools.get_all_paths(sys.argv)
+
+    with open(os.path.join(os.getcwd(), 'idxstats.csv'), 'a') as datafile:
+        # Column headers
+        datafile.write("S#" + "\t" +
+                       "Type" + "\t" +
+                       "chr1 (H3F3A), mapped" + "\t" +
+                       "chr1 (H3F3A), unmapped" + "\t" +
+                       "chr15 (IDH2), mapped" + "\t" +
+                       "chr15 (IDH2), unmapped" + "\t" +
+                       "chr2 (IDH1), mapped" + "\t" +
+                       "chr2 (IDH1), unmapped" + "\t" +
+                       "chr3 (CTNNB1), mapped" + "\t" +
+                       "chr3 (CTNNB1), unmapped" + "\t" +
+                       "chr6 (H3.1), mapped" + "\t" +
+                       "chr6 (H3.1), unmapped" + "\t" +
+                       "chr7 (BRAF), mapped" + "\t" +
+                       "chr7 (BRAF), unmapped" + "\t" +
+                       "other, mapped" + "\t" +
+                       "other, unmapped" + "\t" +
+                       "% mapped" + "\t" +
+                       "% mapped and on target" + "\n")
+        datafile.close()
+
     for dirname in os.listdir(paths["run_path"]):
         sample_path = paths["run_path"] + dirname + "/"
         # To check it is dir, not file
@@ -16,16 +39,12 @@ def main():
 
             if os.path.isdir(data_path):
                 for filename in os.listdir(data_path):
-                    if "_sorted.bam" in filename:
+                    if ("_sorted.bam" in filename and
+                            "_sorted.bam.bai" not in filename):
                         to_write = list()  # to write to .csv file
                         to_write.append(info["sample_num"])
+                        print(info["sample_num"])
                         to_write.append(info["sample_name"])
-
-                        # subset!!!
-                        subprocess_args = [samtools view -Shu -L cranio1.bed S34_sorted.bam > S34_sorted_subset.bam]
-                        index_args = [samtools index S34_sorted_subset.bam]
-
-                        subprocess.check_call()
 
                         popen_args = ["samtools",
                                       "idxstats",
@@ -33,23 +52,43 @@ def main():
 
                         p = subprocess.Popen(popen_args, stdout=subprocess.PIPE)
 
+                        # For calculations
+                        total = 0
+                        total_mapped = 0
+                        total_mapped_on_target = 0
+                        other_mapped = 0
+                        other_unmapped = 0
+
                         while 1:
                             row = p.stdout.readline()
-                            row_list = row.decode('UTF-8').split("\t")
+                            rows = row.decode('UTF-8').split("\t")
+                            row_list = list()
+                            for r in rows:
+                                row_list.append(r.strip())
+
                             if len(row_list) > 1:
-                                if (row_list[1] is not "0"
-                                        or row_list[2] is not "0"):
-                                    mapped = row_list[1]
-                                    unmapped = row_list[2]
-                                    to_write.append(row_list[0])
-                                    to_write.append(mapped)
-                                    to_write.append(unmapped)
-                                    percent = int(mapped) / (int(unmapped) + int(mapped))
-                                    to_write.append("{0:.1f}".format(percent * 100))
+                                if (row_list[0] == "chr1" or row_list[0] == "chr15" or
+                                        row_list[0] == "chr2" or row_list[0] == "chr3" or
+                                        row_list[0] == "chr6" or row_list[0] == "chr7"):
+                                    to_write.append(row_list[2])
+                                    to_write.append(row_list[3])
+                                    total_mapped_on_target += int(row_list[2])
+                                else:
+                                    other_mapped += int(row_list[2])
+                                    other_unmapped += int(row_list[3])
+                                total_mapped += int(row_list[2])
+                                total += int(row_list[2]) + int(row_list[3])
+
                             if not row and p.returncode is not None:
                                 break
                             p.poll()
                         print("done %d" % p.returncode)
+
+                        to_write.append(str(other_mapped))
+                        to_write.append(str(other_unmapped))
+                        if total is not 0:
+                            to_write.append("{0:.1f}".format(total_mapped / total * 100))
+                            to_write.append("{0:.1f}".format(total_mapped_on_target / total * 100))
 
                         with open(os.path.join(os.getcwd(), 'idxstats.csv'), 'a') as datafile:
                             for w in to_write:
@@ -57,10 +96,6 @@ def main():
 
                             datafile.write("\n")
                             datafile.close()
-
-            # Iterate through files, produce idxstats, append to file!
-            # for filename in temppath:
-            #     print("do something")
 
 
 if __name__ == "__main__":
