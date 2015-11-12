2a50b102100ee50156a2243e1a57576b4db334c5
Alice <alicegutteridge@gmail.com>
2015-11-12 15:08:47 +0000

    changed logging, string interpolation

--- batch_align.py
+++ batch_align.py
@@ -14,12 +14,12 @@ def main():
         # To check it is dir, not file
         if os.path.isdir(paths["run_path"] + dirname):
             # BaseSpace (Illumina) directory structure
-            temppath = (paths["run_path"] + dirname +
-                        "/Data/Intensities/BaseCalls/")
+            data_path = (paths["run_path"] + dirname +
+                         "/Data/Intensities/BaseCalls/")
 
             # So that alignments are not done twice
             alignment_done = False
-            for filename in os.listdir(temppath):
+            for filename in os.listdir(data_path):
                 if ".bam.bai" in filename:
                     alignment_done = True
 
@@ -31,22 +31,24 @@ def main():
                     # Sample number
                     sample = ""
 
-                    for filename in os.listdir(temppath):
+                    for filename in os.listdir(data_path):
                         # assumes no non-fastq files will have _R1_ or _R2_ !
                         if "_R1_" in filename:
 
                             if sample is "":
                                 # get sample number between S1-96
                                 sample_re = re.match(
-                                    r"[\S*\s*]*_(S\d*)_[\S*\s*]*",
+                                    r"[\S*\s*]*_(S\d+)_[\S*\s*]*",
                                     filename)
 
                                 if sample_re:
                                     sample = sample_re.group(1)
+                                else:
+                                    log("Regex error in " + filename)
 
                             if "fastq.gz" in filename:
                                 subprocess.check_call(["gunzip",
-                                                       temppath + filename])
+                                                       data_path + filename])
                                 # removes .gz from strings
                                 file1 = filename[:-3]
                             else:
@@ -55,7 +57,7 @@ def main():
                         elif "_R2_" in filename:
                             if "fastq.gz" in filename:
                                 subprocess.check_call(["gunzip",
-                                                       temppath + filename])
+                                                       data_path + filename])
                                 file2 = filename[:-3]
                             else:
                                 file2 = filename
@@ -68,40 +70,34 @@ def main():
                             run_sample is not ""):
 
                         return_code = subprocess.check_call([
-                                            os.cwd() + "/batch_align.sh",
+                                            os.getcwd() + "/batch_align.sh",
                                             file1,
                                             file2,
                                             run_sample,
-                                            temppath,
-                                            os.cwd() + "/ref/hg38bwaidx"])
+                                            data_path])
 
                         if return_code is 0:
                             number_alignments = number_alignments + 1
-
-                            with open(os.path.join('./', 'log.txt'),
-                                      'a') as datafile:
-                                datafile.write(dirname +
-                                               "aligned and indexed.\n")
-                                datafile.write("Alignments so far: " +
-                                               str(number_alignments))
-                                datafile.close()
+                            log(dirname + " aligned successfully.")
 
                         else:
-                            problem(dirname + " with subprocess.check_call.")
+                            log("error no. %s: %s, subprocess.check_call." % (str(return_code), sample))
 
                     else:
-                        problem(dirname +
-                                " with getting sample and file names.")
+                        log("unnamed variable in %s\nfile1: %s\nfile2: %s\nfull sample name: %s" %
+                            (sample, file1, file2, run_sample))
 
                 except Exception as e:
-                    problem(dirname)
-                    print(e)
+                    log("Exception in directory " + dirname)
+                    log(e)
                     sys.exit(0)
 
+    log("Alignments so far: " + str(number_alignments))
 
-def problem(dirname):
-    with open(os.path.join('./', 'log.txt'), 'a') as datafile:
-        datafile.write("agh! problem in " + dirname + "\n")
+
+def log(message):
+    with open(os.path.join(os.getcwd(), 'batch_align_log.txt'), 'a') as datafile:
+        datafile.write(message + "\n")
         datafile.close()
 
 if __name__ == "__main__":
