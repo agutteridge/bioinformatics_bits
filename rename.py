cbed763e2a153633dae42f7dbbea8ee899565560
Alice <alicegutteridge@gmail.com>
2015-11-12 14:30:18 +0000

    Got rid of appending _old_kept to filenames, improved logging, replacing spaces with underscores, making all S#s double digit (e.g. S1 -> S01

--- rename.py
+++ rename.py
@@ -13,6 +13,9 @@
 import os
 import subprocess
 import re
+import sys
+
+import dir_tools
 
 
 def mv_rename(old, new):
@@ -23,7 +26,7 @@ def mv_rename(old, new):
     except:
         log(old + " name not changed")
 
-    print(old + " renamed to " + new)
+    log(old + " renamed to " + new)
 
 
 # error logging
@@ -56,12 +59,12 @@ def main():
     if sample_names and sample_nums:
         print("dictionary populated.")
 
-    root = os.getcwd()
+    paths = dir_tools.get_all_paths(sys.argv)
 
-    for dirname in os.listdir(root):
+    for dirname in os.listdir(paths["run_path"]):
         # To check it is dir, not file
-        if os.path.isdir(root + dirname):
-            bcpath = root + dirname + "/Data/Intensities/BaseCalls/"
+        if os.path.isdir(paths["run_path"] + dirname):
+            bcpath = paths["run_path"] + dirname + "/Data/Intensities/BaseCalls/"
             for filename in os.listdir(bcpath):
                 # Renaming files in directory
                 # get sample number between S1-96
@@ -75,35 +78,36 @@ def main():
                         new_filename = re.sub(r"_S(\d+)",
                                               "_S" + str(new_num),
                                               filename)
-                        mv_rename(bcpath + filename,
-                                  bcpath + new_filename)
+                        if new_filename != filename:
+                            mv_rename(bcpath + filename,
+                                      bcpath + new_filename)
                     except KeyError:
                         log(old_num + " not found in dict!")
                 elif (".sam" in filename or
                       ".bam" in filename or
                       ".fastq" in filename):
-                        mv_rename(bcpath + filename,
-                                  bcpath + filename + "_old_kept")
+                    log(filename + " name not changed")
 
             # Renaming directory
             # Assumes Illumina numeric ID is at least 8 digits long
-            name_re = re.match(r"([\S*\s*]*)-\d\d\d\d\d\d\d\d+\Z",
+            name_re = re.match(r"([\S*\s*]*[A|B])-\d\d\d\d\d\d\d\d+\Z",
                                dirname)
 
             if name_re:
                 old_name = name_re.group(1)
                 try:
                     new_name = sample_names.get(old_name)
-                    new_dirname = re.sub(r"([\S*\s*]*)(-\d\d\d\d\d\d\d\d+)\Z",
+                    new_name = new_name.replace(" ", "_")
+                    new_dirname = re.sub(r"([\S*\s*]*[A|B])(-\d\d\d\d\d\d\d\d+)\Z",
                                          new_name + r"\g<2>",
                                          dirname)
-                    mv_rename(root + dirname,
-                              root + new_dirname)
+                    if dirname != new_dirname:
+                        mv_rename(paths["run_path"] + dirname,
+                                  paths["run_path"] + new_dirname)
                 except KeyError:
                     log(old_name + " not found in dict!")
             else:
-                mv_rename(root + dirname,
-                          root + dirname + "_old_kept")
+                log(dirname + " name not changed")
 
 if __name__ == "__main__":
     main()
